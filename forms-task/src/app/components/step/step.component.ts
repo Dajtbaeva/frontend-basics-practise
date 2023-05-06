dee0bdc0ac4a93485f021b6a24deddb3f75b2d22
Darina <dajtbaeva@gmail.com>
2023-05-06 23:15:54 +0600

    upd

--- forms-task/src/app/components/step/step.component.ts
+++ forms-task/src/app/components/step/step.component.ts
@@ -4,16 +4,16 @@ import { Status, Step } from 'src/app/models/models';
 @Component({
   selector: 'app-step',
   templateUrl: './step.component.html',
-  styleUrls: ['./step.component.scss']
+  styleUrls: ['./step.component.scss'],
 })
-export class StepComponent implements OnInit{
+export class StepComponent implements OnInit {
   steps: Step[] = [];
-  
+
   ngOnInit(): void {
     this.initSteps();
   }
 
-  initSteps(){
+  initSteps() {
     this.steps = [
       {
         Id: 1,
@@ -22,7 +22,7 @@ export class StepComponent implements OnInit{
       },
       {
         Id: 2,
-        Header: 'Check <br> something',
+        Header: 'Check something',
         Status: Status.Active,
       },
       {
@@ -30,8 +30,21 @@ export class StepComponent implements OnInit{
         Header: 'Finish',
         Status: Status.Inactive,
       },
-    ]
+    ];
   }
 
-
+  getClassName(status: Status) {
+    let res = '';
+    switch (status) {
+      case Status.Active:
+        res = 'active';
+        break;
+      case Status.Done:
+        res = 'done';
+        break;
+      case Status.Inactive:
+        res = 'inactive';
+    }
+    return res;
+  }
 }
